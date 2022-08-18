cfb45f532a6bc30c58c5b0496181bd04b78789e0
K5 <utkuzun50@gmail.com>
2022-08-18 17:04:36 +0300

    country hook done

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -11,14 +11,22 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then((response) => setCountry(response.data[0]))
+      .catch((error) => {
+        console.log(error)
+        setCountry(null)
+      })
+  }, [name])
 
   return country
 }
@@ -31,11 +39,15 @@ const Country = ({ country }) => {
   return (
     <div>
       <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
+      <div>population {country.population}</div>
       <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+      <img
+        src={country.flags.png}
+        height='100'
+        alt={`flag of ${country.name.common}`}
+      />
     </div>
-  )  
+  )
 }
 
 const App = () => {
